2307dad49dd230be19e9beb1169a70fb4be48455
Daan Wynen <black.puppydog@gmx.de>
2019-05-07 15:26:25 +0200

    specify python version reqirement.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(name='pytorch_vgg_named',
       description="Named VGG feature extraction",
       long_description="Provides a version of the popular VGG networks that can be used as feature extractors similar to how to query nodes in tensorflow. Useful for style transfer code, where you want to do stuff like r11, r31, r51 = net.forward(targets=['relu1_1', 'relu3_1', 'relu5_1']).",
       long_description_content_type="text/markdown",
+      python_requires='>=3.6',
       install_requires=[
         'torch',
         'torchvision',
