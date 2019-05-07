98a1c34badc89fdb22d07ee602a2718fb48d77cb
Daan Wynen <black.puppydog@gmx.de>
2019-05-07 15:16:59 +0200

    more complete setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,20 @@ from setuptools import setup
 
 setup(name='pytorch_vgg_named',
       version='0.1',
-      description='Provides a version of the popular VGG networks that can be used as feature extractors similar to how to query nodes in tensorflow. Mostly useful for style transfer code.',
       url='http://github.com/black-puppydog/pytorch_vgg_named',
       author='Daan Wynen',
       author_email='daan.wynen@inria.fr',
       packages=['vgg_named'],
-      zip_safe=False)
+      description="Named VGG feature extraction",
+      long_description="Provides a version of the popular VGG networks that can be used as feature extractors similar to how to query nodes in tensorflow. Useful for style transfer code, where you want to do stuff like r11, r31, r51 = net.forward(targets=['relu1_1', 'relu3_1', 'relu5_1']).",
+      long_description_content_type="text/markdown",
+      install_requires=[
+        'torch',
+        'torchvision',
+      ],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+      ],
+      )
